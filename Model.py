4ddf194c426b1dedd3ad0d26f39307f14256f509
Alexander F. I. Osman, M.Sc <alexanderfadul@yahoo.com>
2022-03-12 21:04:25 +0200

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,23 +1,7 @@
-## Deep Learning for MR Image Synthesis
+## Deep Learning for Cross-Sequence MR Image Synthesis
 # @author: Alexander F.I. Osman, April 2021
 
-"""
-This code demonstrates a CNN (Convolutional Encoder-Decoder) using
-BRATS-2018 imaging dataset.
-Training and testing for end-to-end image translation of MRI contrasts
-This code uses 256x256 2D images.
-
-The code goes through the following steps:
-1. load data and pre-process the images (clean data, split , normalize, etc.)
-2. build model architecture
-3. train model and validate its performance
-4. make predictions for previously unseen/test data
-5. evaluate its predictions for previously unseen data
-"""
-
-###############################################################################
-# 1. LOAD DATA AND PREFORM PRE-PROCESSING #####################################
-###############################################################################
+# This code demonstrates a U-Net model for end-to-end MR image translation using BRATS-2018 imaging dataset.
 
 # Import libraries
 import numpy as np
@@ -55,227 +39,96 @@ from keras.optimizers import Adam
 from keras.models import load_model
 from keras.optimizers import Adam
 
-# For consistency
-seed = 42
-np.random.seed = seed
+###############################################################################
+# 1. LOAD DATA AND PREFORM PRE-PROCESSING #####################################
+###############################################################################
 
-# Reading the *nii MR image data files - source and target images
-img_pathS_T1 = 'MR_and_MR_images/train_path/Source_images_T1\Brats18_2013_1_1_t1.nii.gz'
-img_pathT_T2 = 'MR_and_MR_images/train_path/Target_images_T2\Brats18_2013_1_1_t2.nii.gz'
+# Expand the dimension of the array (:,240,240) to (:,240,240,1)
+trainSrc = np.expand_dims(dataSrc_allTrain, -1)
+trainSrc = trainSrc.astype('float32')
 
-dataS_T1 = nib.load(img_pathS_T1)
-dataT_T2 = nib.load(img_pathT_T2)
+trainTrgt = np.expand_dims(dataTrgt_allTrain, -1)
+trainTrgt = trainTrgt.astype('float32')
 
-dataS_T1 = dataS_T1.get_fdata()
-dataT_T2 = dataT_T2.get_fdata()
-
-# Image preprocessing: (A) Get the brain region (dimensionality reduction)
-indice_listS_T1 = np.where(dataS_T1 > 0)
-indice_listT_T2 = np.where(dataT_T2 > 0)
-# calculate the min and max of the indice,  here volume have 3 channels
-# channel_0_min = min(indice_list[0])
-# channel_0_max = max(indice_list[0])
-# channel_1_min = min(indice_list[1])
-# channel_1_max = max(indice_list[1])
-channel_2_minS_T1 = min(indice_listS_T1[2])
-channel_2_maxS_T1 = max(indice_listS_T1[2])
-
-channel_2_minT_T2 = min(indice_listT_T2[2])
-channel_2_maxT_T2 = max(indice_listT_T2[2])
-
-brain_volS_T1 = dataS_T1[:,:,channel_2_minS_T1:channel_2_maxS_T1]
-brain_volT_T2 = dataT_T2[:,:,channel_2_minT_T2:channel_2_maxT_T2]
-
-# Sanity check, view few images
-last = channel_2_maxS_T1 - channel_2_minS_T1
-
-image_number = random.randint(0, last-3)
-print(image_number)
-plt.figure()
-plt.subplot(121)
-plt.imshow(brain_volS_T1[:,:,image_number].T, cmap='gray')
-plt.title('S'), plt.colorbar()
-plt.subplot(122)
-plt.imshow(brain_volT_T2[:,:,image_number].T, cmap='gray')
-plt.title('T'), plt.colorbar()
-plt.show()
-
-"""
-# Image preprocessing: (B) N4 Bias Field Correction by simpleITK
-#This function carry out BiasFieldCorrection for the files in a specific directory
-#src_path: path of the source file
-#dst_path: path of the target file
-
-src_path = 'MR_and_MR_images/train_path/Source_images_T1\Brats18_2013_0_1_t1.nii.gz'
-inputImage = sitk.ReadImage(src_path)
-maskImage = sitk.OtsuThreshold(inputImage, 0, 1, 200)
-inputImage = sitk.Cast(inputImage, sitk.sitkFloat32)
-corrector = sitk.N4BiasFieldCorrectionImageFilter()
-output = corrector.Execute(inputImage, maskImage)
-
-#Sanity check, view few images
-plt.figure()
-plt.subplot(121)
-plt.imshow(output[:,:,image_number].T, cmap='gray')
-plt.title('S'), plt.colorbar()
-plt.subplot(122)
-plt.imshow(output[:,:,image_number].T, cmap='gray')
-plt.title('T'), plt.colorbar()
-plt.show()
-"""
-
-# Image preprocessing: (B) Normalize the data (zero mean and unit variance)
-axis = None
-vol_meanS_T1 = np.mean(brain_volS_T1, axis=axis)
-vol_stdS_T1 = np.std(brain_volS_T1, axis=axis)
-vol_normS_T1 = np.abs((brain_volS_T1 - vol_meanS_T1) / vol_stdS_T1)
-
-vol_meanT_T2 = np.mean(brain_volT_T2, axis=axis)
-vol_stdT_T2 = np.std(brain_volT_T2, axis=axis)
-vol_normT_T2 = np.abs((brain_volT_T2 - vol_meanT_T2) / vol_stdT_T2)
-
-#Sanity check, view few images
-plt.figure()
-plt.subplot(121)
-plt.imshow(vol_normS_T1[:,:,image_number].T, cmap='gray')
-plt.title('S'), plt.colorbar()
-plt.subplot(122)
-plt.imshow(vol_normT_T2[:,:,image_number].T, cmap='gray')
-plt.title('T'), plt.colorbar()
-plt.show()
-
-# Scaling/Normalization: mapping to 0-1
-axis = None
-vol_maxS_T1 = np.max(vol_normS_T1, axis=axis)
-vol_minS_T1 = np.min(vol_normS_T1, axis=axis)
-vol_normS_T1 = (vol_normS_T1 - vol_minS_T1)/(vol_maxS_T1 - vol_minS_T1)
-
-vol_maxT_T2 = np.max(vol_normT_T2, axis=axis)
-vol_minT_T2 = np.min(vol_normT_T2, axis=axis)
-vol_normT_T2 = (vol_normT_T2 - vol_minT_T2)/(vol_maxT_T2 - vol_minT_T2)
-
-#Sanity check, view few images
-plt.figure()
-plt.subplot(121)
-plt.imshow(vol_normS_T1[:,:,image_number].T, cmap='gray')
-plt.title('S'), plt.colorbar()
-plt.subplot(122)
-plt.imshow(vol_normT_T2[:,:,image_number].T, cmap='gray')
-plt.title('T'), plt.colorbar()
-plt.show()
-
-# Image preprocessing: (C) Resizing the images
-width0, height0, depth0 = vol_normS_T1.shape
-
-width1 = 224
-height1 = 224
-depth1 = depth0
-
-resiedS_T1 = np.zeros((depth1, width1, height1))
-resiedT_T2 = np.zeros((depth1, width1, height1))
-
-for i in range (depth1-1):
-    imgS_T1 = vol_normS_T1[:,:,i]
-    imgT_T2 = vol_normT_T2[:,:,i]
-    img_smS_T1 = cv2.resize(imgS_T1, (width1, height1), interpolation=cv2.INTER_CUBIC)
-    img_smT_T2 = cv2.resize(imgT_T2, (width1, height1), interpolation=cv2.INTER_CUBIC)
-    resiedS_T1[i,:,:] = np.abs(img_smS_T1)
-    resiedT_T2[i,:,:] = np.abs(img_smT_T2)
-
-# Sanity check, view few images
-plt.figure()
-plt.subplot(121)
-plt.imshow(resiedS_T1[image_number,:,:].T, cmap='gray')
-plt.title('S'), plt.colorbar()
-plt.subplot(122)
-plt.imshow(resiedT_T2[image_number,:,:].T, cmap='gray')
-plt.title('T'), plt.colorbar()
-plt.show()
+X_train, X_test, Y_train, Y_test = train_test_split(trainSrc, trainTrgt, test_size=0.20, random_state=1)
+X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.20, random_state=1)
 
 ###############################################################################
 # 2. BUILD THE MODEL ARCHITECTURE #############################################
 ###############################################################################
 
-#vol_maxS_T1 = print(np.min(resiedS_T1))
-#vol_maxT_T2 = print(np.min(resiedT_T2))
-width1 = 224
+# For consistency
+seed = 42
+np.random.seed = seed
+
 height1 = 224
+width1 = 224
 channel1 = 1
-depth1 = depth0
-
-trainS_T1 = np.expand_dims(resiedS_T1, -1)
-trainS_T1 = trainS_T1.astype('float32')
-
-trainT_T2 = np.expand_dims(resiedT_T2, -1)
-trainT_T2 = trainT_T2.astype('float32')
-
-X_train, X_test, Y_train, Y_test = train_test_split(trainS_T1, trainT_T2, test_size=0.20, random_state=1)
-X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.20, random_state=1)
-
-start = time.time()
 
 inputs = Input((height1, width1, channel1))
 s = inputs
-#LeakyReLU
 #Contraction path
 c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(s)
-c1 = Dropout(0.1)(c1)
+#c1 = Dropout(0.1)(c1)
 c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c1)
 p1 = MaxPooling2D((2, 2))(c1)
 
 c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(p1)
-c2 = Dropout(0.1)(c2)
+#c2 = Dropout(0.1)(c2)
 c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c2)
 p2 = MaxPooling2D((2, 2))(c2)
 
 c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(p2)
-c3 = Dropout(0.2)(c3)
+#c3 = Dropout(0.2)(c3)
 c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c3)
 p3 = MaxPooling2D((2, 2))(c3)
 
 c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(p3)
-c4 = Dropout(0.2)(c4)
+#c4 = Dropout(0.2)(c4)
 c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c4)
 p4 = MaxPooling2D(pool_size=(2, 2))(c4)
 
 c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(p4)
-c5 = Dropout(0.3)(c5)
+#c5 = Dropout(0.3)(c5)
 c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c5)
 
 #Expansive path
-u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
-#u6 = UpSampling2D(size=(2, 2))(c5)
+#u6 = Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(c5)
+u6 = UpSampling2D(size=(2, 2))(c5)
 u6 = concatenate([u6, c4])
 c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(u6)
 c6 = Dropout(0.2)(c6)
 c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c6)
 
-u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
-#u7 = UpSampling2D(size=(2, 2))(c6)
+#u7 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c6)
+u7 = UpSampling2D(size=(2, 2))(c6)
 u7 = concatenate([u7, c3])
 c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(u7)
 c7 = Dropout(0.2)(c7)
 c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c7)
 
-u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
-#u8 = UpSampling2D(size=(2, 2))(c7)
+#u8 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c7)
+u8 = UpSampling2D(size=(2, 2))(c7)
 u8 = concatenate([u8, c2])
 c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(u8)
 c8 = Dropout(0.1)(c8)
 c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c8)
 
-u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
-#u9 = UpSampling2D(size=(2, 2))(c8)
+#u9 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c8)
+u9 = UpSampling2D(size=(2, 2))(c8)
 u9 = concatenate([u9, c1], axis=3)
 c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(u9)
 c9 = Dropout(0.1)(c9)
 c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same')(c9)
 
-outputs = Conv2D(1, (1, 1), activation='sigmoid')(c9)
+outputs = Conv2D(1, (1, 1), activation='tanh')(c9)
 
 model = Model(inputs=[inputs], outputs=[outputs])
-model.compile(optimizer=Adam(learning_rate=0.0001), loss='mean_squared_error', metrics=['mae'])
-model.summary()
+model.compile(optimizer=Adam(learning_rate=0.001), loss='mean_squared_error', metrics=['mae'])
+#Summerize layers
+print(model.summary())
+#Plot graph
+#plot_model(model, to_file='simple_model.png')
 
 ###############################################################################
 # 3. TRAIN AND VALIDATE THE CNN MODEL #########################################
@@ -286,13 +139,13 @@ callbacks = tf.keras.callbacks.EarlyStopping(patience=30, monitor='val_loss', ve
 # The patience parameter is the amount of epochs to check for improvement
 
 # Train the model
+start = time.time()
 history = model.fit(X_train, Y_train,
                     batch_size=32,
-                    epochs=150,
+                    epochs=120,
                     verbose=1,
                     callbacks=[callbacks],
-                    validation_data=(X_val, Y_val),
-                    shuffle=True)
+                    validation_data=(X_val, Y_val))
 
 finish = time.time()
 print('total_time = ', finish - start)
@@ -314,12 +167,6 @@ plt.show()
 model.save('saved_model/MRI_Synthesis_T1_T2')
 #model.save('MRI_Synthesis_T1_T2.h5')
 
-# Predict model on the training/same input array.
-predTrain = model.predict(X_train, verbose = 1)
-predTrain = np.reshape(predTrain, (len(predTrain), width1, height1))
-
-gtTrain = np.reshape(Y_train, (len(Y_train), width1, height1))
-
 ###############################################################################
 # 4. MAKE PREDICTIONS ON TEST DATASET #########################################
 ###############################################################################
@@ -337,55 +184,81 @@ predTest = np.reshape(predTest, (len(predTest), width1, height1))
 gtTest = np.reshape(Y_test, (len(Y_test), width1, height1))
 
 # Sanity check, view few images
-image_number = random.randint(0, len(Y_test-3))
+image_number = random.randint(0, len(Y_test-1))
 #print(image_number)
 plt.figure()
 plt.subplot(241)
-plt.imshow(predTest[image_number,:,:].T, cmap='gray')
-plt.title('Predicted Test'), #plt.clim(0, 1), plt.colorbar()
+plt.imshow(predTest[image_number,:,:], cmap='gray')
+plt.title('Predicted Test'), #plt.clim(0, 1)
+plt.colorbar()
 plt.subplot(245)
-plt.imshow(gtTest[image_number,:,:].T, cmap='gray')
-plt.title('GT'), #plt.clim(0, 1), plt.colorbar()
-
-image_number = random.randint(0, len(Y_test-3))
-plt.subplot(242)
-plt.imshow(predTest[image_number,:,:].T, cmap='gray')
-plt.title('Predicted Test'), #plt.clim(0, 1), plt.colorbar()
-plt.subplot(246)
-plt.imshow(gtTest[image_number,:,:].T, cmap='gray')
-plt.title('GT'), #plt.clim(0, 1), plt.colorbar()
-
-image_number = random.randint(0, len(Y_test-3))
-plt.subplot(243)
-plt.imshow(predTest[image_number,:,:].T, cmap='gray')
-plt.title('Predicted Test'), #plt.clim(0, 1), plt.colorbar()
-plt.subplot(247)
-plt.imshow(gtTest[image_number,:,:].T, cmap='gray')
-plt.title('GT'), #plt.clim(0, 1), plt.colorbar()
-
-image_number = random.randint(0, len(Y_test-3))
-plt.subplot(244)
-plt.imshow(predTest[image_number,:,:].T, cmap='gray')
-plt.title('Predicted Test'), #plt.clim(0, 1), plt.colorbar()
-plt.subplot(248)
-plt.imshow(gtTest[image_number,:,:].T, cmap='gray')
-plt.title('GT'), #plt.clim(0, 1), plt.colorbar()
-plt.show()
+plt.imshow(gtTest[image_number,:,:], cmap='gray')
+plt.title('GT'), #plt.clim(0, 1)
+plt.colorbar()
 
 ###############################################################################
 # 5. EVALUATE THE PREDICTIONS ON TEST DATASET #########################################
 ###############################################################################
 
-ref_img = gtTest[5,:,:]
+ref_img = gtTest
+eval_img = predTest
+
+_psnr = np.zeros(len(ref_img))
+_ssim = np.zeros(len(ref_img))
+_mse = np.zeros(len(ref_img))
+_mae = np.zeros(len(ref_img))
+
+for j in range (len(ref_img)):
+    # PSNR: Compute the peak signal to noise ratio (PSNR) for an image.
+    _psnr[j] = skimage.metrics.peak_signal_noise_ratio(ref_img[j], eval_img[j],data_range=None)
+    # SSIM: Compute the mean structural similarity index between two images.
+    ssim = skimage.metrics.structural_similarity(ref_img[j], eval_img[j], full=True)
+    _ssim[j] = ssim[0]
+    # MSE: Compute the mean-squared error between two images.
+    _mse[j] = skimage.metrics.mean_squared_error(eval_img[j], ref_img[j])
+    # AE & MAE: Compute the mean absolute error between two images.
+    _mae[j] = sklearn.metrics.mean_absolute_error(ref_img[j], eval_img[j])
+
+average_psnr = np.average(_psnr)
+std_psnr = np.std(_psnr)
+average_ssim = np.average(_ssim)
+std_ssim = np.std(_ssim)
+average_mse = np.average(_mse)
+std_mse = np.std(_mse)
+average_mae = np.average(_mae)
+std_mae = np.std(_mae)
+
+print(f"average_PSNR value is {average_psnr} dB")
+print(f"std_PSNR value is {std_psnr} dB")
+print("average_SSIM of input noisy image = ", average_ssim)
+print("std_SSIM of input noisy image = ", std_ssim)
+print("average_MSE of input noisy image = ", average_mse)
+print("std_MSE of input noisy image = ", std_mse)
+print("average_MAE of input noisy image = ", average_mae)
+print("std_MAE of input noisy image = ", std_mae)
+
+###############
+ref_img = gtTest[125,:,:]
 #ref_img = img_as_ubyte(ref_img)
 print(ref_img.dtype)
 
-eval_img = predTest[5,:,:]
+eval_img = predTest[125,:,:]
 print(eval_img.dtype)
+
+plt.figure()
+plt.subplot(121)
+plt.imshow(ref_img, cmap='gray')
+plt.title('Predicted Test'), #plt.clim(0, 1)
+plt.colorbar()
+plt.subplot(122)
+plt.imshow(eval_img, cmap='gray')
+plt.title('GT'), #plt.clim(0, 1)
+plt.colorbar()
+plt.show()
 
 # PSNR: Compute the peak signal to noise ratio (PSNR) for an image.
 psnr = skimage.metrics.peak_signal_noise_ratio(ref_img, eval_img,data_range=None)
-print("PSNR of input noisy image = ", psnr)
+#print("PSNR of input noisy image = ", psnr)
 print(f"PSNR value is {psnr} dB")
 
 # SSIM: Compute the mean structural similarity index between two images.
@@ -393,9 +266,10 @@ ssim = skimage.metrics.structural_similarity(ref_img, eval_img, full=True)
 print("SSIM of input noisy image = ", ssim[0])
 
 plt.figure()
-plt.imshow(ssim[1].T, cmap='gray')
+plt.imshow(ssim[1], cmap='gray')
 plt.title('ssim'), plt.clim(0, 1)
 plt.colorbar()
+plt.axis('off')
 plt.show()
 
 # MSE: Compute the mean-squared error between two images.
@@ -406,19 +280,14 @@ print("MSE of input noisy image = ", mse)
 mae = sklearn.metrics.mean_absolute_error(ref_img, eval_img)
 print("MAE of input noisy image = ", mae)
 
-abs_diff_resid = np.abs(eval_img - ref_img)
+abs_diff = np.abs(eval_img - ref_img)
 
 plt.figure()
-plt.imshow(abs_diff_resid.T, cmap='gray')
+plt.imshow(abs_diff, cmap='gray')
 plt.title('absolute difference map (residual)'), plt.clim(0, 1)
 plt.colorbar()
+plt.axis('off')
 plt.show()
 
-
 ###############################################################################
 ###############################################################################
-###############################################################################
-###############################################################################
-###############################################################################
-
-
